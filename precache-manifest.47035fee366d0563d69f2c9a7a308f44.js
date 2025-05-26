9177fabc303aca57a62559dddcdef9896a23e792
Libra-lsp <lishipeng1012@outlook.com>
2025-05-26 06:39:34 +0000

    Deploying to gh-pages from @ thingraph/official-website@8e39ba53b586460cbe285b67db9409922bf3b2fd 🚀

---
+++ precache-manifest.47035fee366d0563d69f2c9a7a308f44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f63d3e59b573be09eab6",
+    "url": "/static/css/main.c66db43b.chunk.css"
+  },
+  {
+    "revision": "f63d3e59b573be09eab6",
+    "url": "/static/js/main.f63d3e59.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "0f73b92400abce101b26",
+    "url": "/static/css/2.d0ab5ab8.chunk.css"
+  },
+  {
+    "revision": "0f73b92400abce101b26",
+    "url": "/static/js/2.0f73b924.chunk.js"
+  },
+  {
+    "revision": "9ad85d957e8c0c46f9316bc53b502279",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.10311965c4325a5b6812ed4b25beb327.js"
+  "/precache-manifest.47035fee366d0563d69f2c9a7a308f44.js"
 );
 
 workbox.clientsClaim();
